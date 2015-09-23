958d131266eb43fdeebc42a9ba59e319712e328a
Rodrigo Jeria <wiwocs2186@gmail.com>
2015-09-23 14:33:25 -0300

    -Creacion de la estructura basica para el ejercicio n°2 de bodega, incluye jquery, plugins y una tabla base.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bodega</title>
+		<meta charset="utf-8">
+		<script src="js/jquery-2.1.4.js"></script>
+		<script src="js/jquery.colorbox.js"></script>
+		<script src="js/jquery.colorbox-es.js"></script>
+		<script src="js/jquery.tablesorter.js"></script>
+		<link rel="stylesheet" href="css/estilos.css" type="text/css" media="all"/>
+		<script>
+			$(document).ready(function(){
+				
+			})
+		</script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<h1>Bodega 2</h1>
+			<table border="1">
+				<thead>
+					<tr>
+						<th>Bodega</th>
+						<th>Producto</th>
+						<th>Precio</th>
+						<th>Stock</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>(bodega)</td>
+						<td>(producto)</td>
+						<td>(precio)</td>
+						<td>(stock)</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</body>
+</html>
